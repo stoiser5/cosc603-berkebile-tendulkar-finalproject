e0212571121be7c41659cf3aab731c5236055d09
mtendu <madhuratendulkar09@gmail.com>
2016-05-15 21:35:06 -0400

    Added test cases:
    1. testHasBeenTraded()
    2. testGetInitialPrice()
    3. testSetInitialPrice()
    4. testSetAddress()
    5. testSetArreas()
    6. testModifyIncomeAfterTax()
    7. testModifyIncomeBeforeTax()
    8. testUpdate()
    9. testSetOwner()
    10. testGetAmountInMarket()

--- FreeCol/test/src/net/sf/freecol/common/model/MarketTest.java
+++ FreeCol/test/src/net/sf/freecol/common/model/MarketTest.java
@@ -44,7 +44,170 @@ public class MarketTest extends FreeColTestCase {
             assertEquals(good.toString(), good.getInitialSellPrice(), dm.getPaidForSale(good));
         }
     }
+    public void testHasBeenTraded(){
+    	
+    	Game game = getStandardGame();
 
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+
+        Market market = player.getMarket();
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        goodsType.setPrice(10);
+        goodsType.setId("1");
+        
+        assertFalse(market.hasBeenTraded(goodsType));
+    }
+    
+    public void testGetInitialPrice(){
+    	
+    	Game game = getStandardGame();
+
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+
+        Market market = player.getMarket();
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        assertEquals(0, market.getInitialPrice(goodsType));
+    	
+    }
+    
+    public void testSetInitialPrice(){
+    	
+    	Game game = getStandardGame();
+
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+
+        Market market = player.getMarket();
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        market.setInitialPrice(goodsType, 100);
+        assertEquals(100, market.getInitialPrice(goodsType));
+    	
+    }
+    
+    public void testGetBidPrice(){
+    	Game game = getStandardGame();
+
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        Market market = player.getMarket();
+        
+        assertEquals(0, market.getBidPrice(goodsType, 100));
+        
+    	
+    	
+    }
+    
+    public void testSetArrears(){
+    	Game game = getStandardGame();
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        Market market = player.getMarket();
+        market.setArrears(goodsType, 100);
+        assertEquals(100, market.getArrears(goodsType));
+    	
+    }
+    
+    public void testModifyIncomeAfterTaxes(){
+    	
+    	Game game = getStandardGame();
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        Market market = player.getMarket();
+        market.modifyIncomeAfterTaxes(goodsType, 100);
+        assertEquals(100, market.getIncomeAfterTaxes(goodsType));
+    	
+    }
+    
+    	public void testModifyIncomeBeforeTaxes(){
+    	
+    	Game game = getStandardGame();
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        Market market = player.getMarket();
+        market.modifyIncomeBeforeTaxes(goodsType, 100);
+        assertEquals(100, market.getIncomeBeforeTaxes(goodsType));
+    	
+    }
+    	
+    public void testUpdate(){
+    	Game game = getStandardGame();
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        Market market = player.getMarket();
+        goodsType.setPrice(100);
+        market.update(goodsType);
+        assertEquals(100, goodsType.getPrice());
+    	
+    	
+    }
+    
+    public void testSetOwner(){
+    	
+    	Game game = getStandardGame();
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Market market = player.getMarket();
+        market.setOwner(player);
+        assertEquals(player, market.getOwner());
+    	
+    }
+    
+    public void testGetAmountInMarket(){
+    	
+    	Game game = getStandardGame();
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Market market = player.getMarket();
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        market.addGoodsToMarket(goodsType, 200);
+        assertEquals(200, market.getAmountInMarket(goodsType));
+        
+    }
+    public void testGetAmountInMarket1(){
+    	
+    	Game game = getStandardGame();
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Market market = player.getMarket();
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        assertEquals(0, market.getAmountInMarket(goodsType));
+        
+    }
+//    
+    public void testModifySales(){
+    	
+    	Game game = getStandardGame();
+        Player player = game.getPlayerByNationId("model.nation.dutch");
+        Market market = player.getMarket();
+        Specification specification = spec("freeCol");
+        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+        
+        market.addGoodsToMarket(goodsType, 200);
+        market.modifySales(goodsType, 100);
+        assertEquals(100, market.getSales(goodsType));
+    	
+    	
+    }
+    
+//    public void testGetSalesPrice(){
+//    	
+//    	Game game = getStandardGame();
+//        Player player = game.getPlayerByNationId("model.nation.dutch");
+//        Market market = player.getMarket();
+//        Specification specification = spec("freeCol");
+//        GoodsType goodsType = new GoodsType(specification.getId(), specification);
+//        market.addGoodsToMarket(goodsType, 200);
+//        
+//        assertEquals(250, market.getSalePrice(goodsType,100));
+//    	
+//    }
+//    
     /**
      * Serialization and deserialization?
      */
